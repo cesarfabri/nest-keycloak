3738f8cfd895a6a11925fd65d0a3d5a2b3b5b535
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-13 17:17:09 -0300

    Refact(login):Refactored login screen for keycloak

--- next/src/pages/login.tsx
+++ next/src/pages/login.tsx
@@ -1,39 +1,25 @@
-import { FormEvent } from "react";
-import styles from "../styles/LoginPage.module.css";
-import { setCookie } from "../util/cookies";
-import { http } from "../util/http";
-import {useRouter} from 'next/router';
+import { useKeycloak } from "@react-keycloak/ssr";
+import { KeycloakInstance } from "keycloak-js";
+import { useEffect } from "react";
+
 
 const LoginPage = () => {
+  const {initialized, keycloak} = useKeycloak<KeycloakInstance>();
+  
+  const { login = () => {}, authenticated } = keycloak || {};
 
-  const router = useRouter();
+  useEffect(() => {
+    if (!initialized) {
+      return
+    }
+    if (!authenticated) {
+      login({
+        redirectUri: 'http://localhost:3007/private'
+      })
+    }
+  }, [login, authenticated, initialized])
 
-  async function onSubmit(event: FormEvent) {
-    event.preventDefault();
-
-    const username = (document.querySelector("#username") as HTMLInputElement)
-      .value;
-    const password = (document.querySelector("#password") as HTMLInputElement)
-      .value;
-
-    const { data } = await http.post("login", { username, password });
-    setCookie("token", data.token);
-    router.push('/private')
-  }
-
-  return (
-    <form method="post" onSubmit={onSubmit}>
-      <div className={styles.formGroup}>
-        <label htmlFor="username">Usuário</label>
-        <input type="text" id="username" name="username" />
-      </div>
-      <div className={styles.formGroup}>
-        <label htmlFor="password">Senha</label>
-        <input type="password" id="password" name="password" />
-      </div>
-      <button type="submit">Login</button>
-    </form>
-  );
+  return null
 };
 
 export default LoginPage;
