8fae774751521214ad9a0440004a55de7e9baa9b
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-13 15:42:54 -0300

    Feat(cookies): applied cookies

--- next/src/pages/_app.tsx
+++ next/src/pages/_app.tsx
@@ -1,6 +1,22 @@
-import type { AppProps } from 'next/app'
+import type { AppContext, AppProps } from 'next/app';
+import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr'
+import { KEYCLOAK_CONFIG } from '../util/auth';
+import { parseCookies } from '../util/cookies';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, cookies }: AppProps & { cookies: any}) {
+  return (
+    <SSRKeycloakProvider 
+      keycloakConfig={KEYCLOAK_CONFIG as any}
+      persistor={SSRCookies(cookies)}
+    >
+      <Component {...pageProps} />
+    </SSRKeycloakProvider>
+  );
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  return {
+    cookie: parseCookies(appContext.ctx.req)
+  }
 }
 export default MyApp
