a9d8c106025ff5bd16392288434b46e8e605addf
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-13 00:04:09 -0300

    Feat(transactions): Added resource transactions

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { TransactionsModule } from './transactions/transactions.module';
 //decorator - Javascript - Ecmascript 7
 
 @Module({
@@ -10,7 +12,22 @@ import { AuthModule } from './auth/auth.module';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    AuthModule],
+    // SequelizeModule.forRoot({
+    //   dialect: process.env.DB_CONNECTION as any,
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   models: [],
+    //   autoLoadModels: true,
+    //   synchronize: true,
+    //   sync: {
+    //     alter: true,
+    //   }
+    // }),
+    AuthModule,
+    TransactionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
