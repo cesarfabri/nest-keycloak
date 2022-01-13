d925dccc883725f3f6c9cb579ec0b00fc3a5f034
Cesar Fabri <cesar.fabrii@gmail.com>
2022-01-13 14:06:24 -0300

    Refact(next): Changed frontend ports

--- next/src/util/http.ts
+++ next/src/util/http.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getCookie } from "./cookies";
 
 export const http = axios.create({
-  baseURL: "http://host.docker.internal:3000",
+  baseURL: "http://host.docker.internal:3006",
 });
 
 http.interceptors.request.use((request) => {
